febf424d93b9e1ae643dbd1d148493274e3bde70
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-07 22:50:22 -0400

    Add do now exercise

---
+++ profile.js
@@ -0,0 +1,16 @@
+// HTML selectors
+let button = document.querySelector("button");
+let profileName = document.querySelector(".profile-name");
+let profileImage = document.querySelector(".profile-image");
+let main = document.querySelector(".main");
+
+button.addEventListener("click", function() {
+
+    // 1. Updated the innerHTML of profileName to your name or a made up one.
+
+    // 2. Updated the src of profileImage a picture of you or one that makes sense.
+
+    // 3. Updated the src of main to a photo that's be your first post.
+
+
+});
